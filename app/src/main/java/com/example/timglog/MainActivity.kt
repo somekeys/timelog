7e2ea36e264da792b9a986ddedcc1097503f4f9d
Yuze Hu <forioophu@gmail.com>
2021-01-16 11:39:49 -0500

    save task statu

--- app/src/main/java/com/example/timglog/MainActivity.kt
+++ app/src/main/java/com/example/timglog/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -56,6 +57,13 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+
+        val taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        if(taskViewModel.isRunning()){
+            navController.navigate(R.id.action_nav_home_to_nav_task)
+        }
+
+
 
 
 
--- app/src/main/java/com/example/timglog/TaskViewModel.kt
+++ app/src/main/java/com/example/timglog/TaskViewModel.kt
@@ -2,7 +2,6 @@ package com.example.timglog
 
 import android.app.Application
 import android.graphics.Typeface
-import android.os.AsyncTask
 import android.os.Handler
 import android.text.SpannableString
 import android.text.style.RelativeSizeSpan
@@ -11,12 +10,12 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import androidx.preference.PreferenceManager.getDefaultSharedPreferences
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.*
 import kotlin.math.abs
 import kotlin.math.log10
-import kotlin.time.milliseconds
 
 class TaskViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -25,6 +24,8 @@ class TaskViewModel(application: Application) : AndroidViewModel(application) {
 
     private var running : Boolean = false
     val handler : Handler = Handler()
+    val prefs =getDefaultSharedPreferences(getApplication())
+
 
     val alltasks: LiveData<List<Task>>
     val categories: LiveData<List<String>>
@@ -33,7 +34,11 @@ class TaskViewModel(application: Application) : AndroidViewModel(application) {
     val category : MutableLiveData<String> = MutableLiveData()
     var startTime:MutableLiveData<Long> = MutableLiveData()
     val duration_text : MutableLiveData<SpannableString>   = MutableLiveData()
-    var nameTaskMap = hashMapOf<String,Task>()
+    var nameTaskMap = hashMapOf<String, Task>()
+    enum class TaskState{
+        STARTED,PAUSED,STOPPED
+    }
+    var stopwatchState : TaskState = TaskState.STOPPED
 
     private var seconds : Int = 0
 
@@ -41,20 +46,29 @@ class TaskViewModel(application: Application) : AndroidViewModel(application) {
     private var onTomato :Boolean = false
     private var numTomato = 0
 
-
     init {
         val tasksDao = TimeLogDatabase.getDatabase(application).taskDao()
         repository = TaskRepository(tasksDao)
         alltasks = repository.allTasks
         categories = repository.cats
-
-        duration_text.value = SpannableString("")
-        startTime.value = System.currentTimeMillis()/1000
-        title.value = "Foo"
-        category.value = "foo"
-
-
+        updateStopwatch()
     }
+
+    fun isRunning(): Boolean {
+        return running
+    }
+    fun updateStopwatch(){
+        startTime.value = prefs.getLong("sw_start_time" , System.currentTimeMillis()/1000)
+        title.value = prefs.getString("sw_title","Foo")
+        category.value = prefs.getString("sw_category","Foo")
+        stopwatchState = TaskState.values()[prefs.getInt("sw_state",TaskState.STOPPED.ordinal)]
+
+        if(stopwatchState == TaskState.STARTED){
+            running = true
+            runTimer()
+        }
+    }
+
 
     fun deleteTask(task: Task){
         viewModelScope.launch(Dispatchers.IO) {
@@ -62,34 +76,37 @@ class TaskViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
-    fun getLastByName(name:String): Task? {
+    fun getLastByName(name: String): Task? {
         return nameTaskMap[name]
     }
 
-    fun startTask( task_title : String, task_category : String ){
+    fun startTask(task_title: String, task_category: String){
         if(running){stopTask()}
-        title.value = task_title
-        category.value = task_category
-        startTime.value = System.currentTimeMillis()/1000
-        seconds = 0
+        val editor = prefs.edit()
+//        title.value = task_title
+//        category.value = task_category
+//        startTime.value = System.currentTimeMillis()/1000
+//        seconds = 0
         running = true
-        runTimer()
-
+        editor.putLong("sw_start_time", System.currentTimeMillis()/1000)
+            .putString("sw_title", task_title)
+            .putString("sw_category", task_category)
+            .putInt("sw_state",TaskState.STARTED.ordinal)
+        editor.apply()
+        updateStopwatch()
     }
 
 
 
     private fun runTimer(){
-        handler.post( object :Runnable {
+        handler.post(object : Runnable {
 
 
-            override fun  run()
-            {
+            override fun run() {
 
-                duration_text.value = secondsToSpaning(seconds)
+                duration_text.value = secondsToSpaning((System.currentTimeMillis()/1000 - startTime.value!!).toInt())
 
                 if (running) {
-                    seconds++;
                     handler.postDelayed(this, 1000)
 
                 }
@@ -102,8 +119,12 @@ class TaskViewModel(application: Application) : AndroidViewModel(application) {
     fun stopTask( ){
         running = false
         handler.removeCallbacksAndMessages(null)
-        insert(Task(0,title.value!!,category.value!!,startTime.value!!,
-            System.currentTimeMillis()/1000 ))
+        insert(
+            Task(
+                0, title.value!!, category.value!!, startTime.value!!,
+                System.currentTimeMillis() / 1000
+            )
+        )
 
     }
 
